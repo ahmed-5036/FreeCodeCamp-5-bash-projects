aede037708b5f7ba2ea9cc10fdc922ccd8498ce0
Ahmed maher <ahmed.sabra@ejust.edu.eg>
2023-06-22 08:43:40 +0300

    Update five.sh

--- five.sh
+++ five.sh
@@ -5,3 +5,40 @@
 ./countdown.sh 3
 ./bingo.sh
 ./fortune.sh
+
+: '
+ Run example:
+ What's your name?
+ Ahmed
+ Hello Ahmed.
+ Where are you from?
+ Alexandria
+ Hello Ahmed from Alexandria.
+ What's your favorite coding website?
+ FCC
+
+ Hello Ahmed from Alexandria. I learned that your favorite coding website is FCC!
+
+ ~~ Countdown Timer ~~
+
+ 3
+ 2
+ 1
+ 0
+ 
+ ~~ Bingo Number Generator ~~
+
+ The next number is, I:19
+
+ ~~ Fortune Teller ~~
+
+ Ask a yes or no question:
+ go to the gym
+ Try again. Make sure it ends with a question mark:
+ go to the gym ?
+
+ Outlook good
+
+
+
+'
